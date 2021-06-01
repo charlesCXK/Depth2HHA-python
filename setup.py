bf48eb6bcad1e6730e9d98249b416088fe4396cb
Catarina Carvalho <ccarvalho@deeperinsights.com>
2021-06-01 10:20:28 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Depth2HHA-python',
+    version='0',
+    packages=[''],
+    url='https://github.com/skimit/Depth2HHA-python',
+    license='MIT License ',
+    author='Chen XiaoKang ',
+    author_email='pkucxk@pku.edu.cn',
+    description='This repo implements HHA-encoding algorithm in python3. HHA image is an encoding '
+                'algorithm to make better use of depth images, which was proposed by s-gupta in '
+                'this paper: Learning Rich Features from RGB-D Images for Object Detection and '
+                'Segmentation.'
+    )
